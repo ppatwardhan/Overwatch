47726d40e037e2a78a2ed069b72a8e043dcfd195
ppatwardhan <ppatwardhan829@gmail.com>
2019-05-06 13:17:11 -0400

    Demo-able website

--- Overwatch.js
+++ Overwatch.js
@@ -1,27 +1,35 @@
-$('document').ready(function () {
+$('document').ready(function() {
 
     function getData(player, hero) {
         fetch('https://ow-api.com/v1/stats/pc/us/' + player + '/heroes/' + hero)
-            .then(function (resp) { return resp.json() }) // Convert data to json
-            .then(function (data) {
+            .then(function(resp) { return resp.json() }) // Convert data to json
+            .then(function(data) {
                 console.log(data);
-                fillTable(data, hero);
+                fillTable(data, hero, player);
 
             })
-            .catch(function () {
+            .catch(function() {
                 // catch any errors
             });
     }
-    $("#searchform").submit(function (e) {
+    $("#searchform").submit(function(e) {
         e.preventDefault();
         getData($('#searchPlayer').val(), $('#hero option:selected').text());
-    
+
     });
 
-    function fillTable(data, hero) {
+    function fillTable(data, hero, player) {
         console.log('empty');
         var dmg = data["competitiveStats"]['careerStats'][hero]['average']['allDamageDoneAvgPer10Min'];
-        console.log(dmg);
-        $("#left-player-dmg").text(dmg);
+        var death = data["competitiveStats"]['careerStats'][hero]['average']['deathsAvgPer10Min'];
+        var obj = data["competitiveStats"]['careerStats'][hero]['combat']['objectiveTime'];
+        var acc = data["competitiveStats"]['careerStats'][hero]['combat']['weaponAccuracy'];
+        var crit = data["competitiveStats"]['careerStats'][hero]['combat']['criticalHits'];
+        $('#your-player').text(player);
+        $("#your-player-dmg").text(dmg);
+        $("#your-player-death").text(death);
+        $("#your-player-obj").text(obj);
+        $("#your-player-acc").text(acc);
+        $("#your-player-crit").text(crit);
     }
 })
