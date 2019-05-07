17384dda3accda728876071218e5e32b3548608d
ppatwardhan <ppatwardhan829@gmail.com>
2019-05-07 10:57:07 -0400

    Finalized demo version

--- Overwatch.js
+++ Overwatch.js
@@ -31,5 +31,21 @@ $('document').ready(function() {
         $("#your-player-obj").text(obj);
         $("#your-player-acc").text(acc);
         $("#your-player-crit").text(crit);
+        $('#first-pro-dmg').text('21.94');
+        $('#second-pro-dmg').text('23.44');
+        $('#third-pro-dmg').text('25.61');
+        $('#first-pro-death').text('.01');
+        $('#second-pro-death').text('.03');
+        $('#third-pro-death').text('.01');
+        $('#first-pro-obj').text('01:17:07');
+        $('#second-pro-obj').text('01:18:22');
+        $('#third-pro-obj').text('01:19:51');
+        $('#first-pro-acc').text('32%');
+        $('#second-pro-acc').text('25%');
+        $('#third-pro-acc').text('41%');
+        $('#first-pro-crit').text('8146');
+        $('#second-pro-crit').text('8567');
+        $('#third-pro-crit').text('9122');
+
     }
 })
